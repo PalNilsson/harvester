9bccd6468ff5547cf6a483890580ac3db96be9b7
Paul Nilsson <palnilsson70@gmail.com>
2023-03-17 16:17:20 +0100

    Updated nfs server IP

--- pandaharvester/harvestersubmitter/dask_submitter.py
+++ pandaharvester/harvestersubmitter/dask_submitter.py
@@ -72,7 +72,7 @@ class DaskSubmitter(PluginBase):
     _password = None
     _interactive_mode = True
     _tmpdir = ''
-    _nfs_server = "10.226.152.66"
+    _nfs_server = "10.132.0.82"  # "10.226.152.66"
     _project = "gke-dev-311213"
     _zone = "europe-west1-b"
     _local_workdir = ''
