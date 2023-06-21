6e5754c3eeb74a5cf24d0834b8c4420717f0fbd1
Paul Nilsson <paul.nilsson@cern.ch>
2023-06-21 18:25:46 +0200

    Testing jupyter port value

--- pandaharvester/harvestermisc/dask_utils.py
+++ pandaharvester/harvestermisc/dask_utils.py
@@ -734,7 +734,7 @@ spec:
             - /bin/bash
             - -c
             - |
-              start.sh jupyter lab --ServerApp.token='CHANGE_PASSWORD' --ServerApp.ip='0.0.0.0' --ServerApp.allow_root=True
+              start.sh jupyter lab --ServerApp.token='CHANGE_PASSWORD' --ServerApp.ip='0.0.0.0' --ServerApp.allow_root=True --ServerApp.port='8888'
           volumeMounts:
             - name: fileserver-CHANGE_USERID
               mountPath: CHANGE_NFS_PATH
